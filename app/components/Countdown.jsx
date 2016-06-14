5d9c21ecb98020a158cc448fc5f964b3e0d3341f
Nate Kimball <nsktrombone84@hotmail.com>
2016-06-14 11:30:49 -0700

    Finished countdown component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -28,6 +28,11 @@ var Countdown = React.createClass({
         }
     },
 
+    componentWillUnmount: function(){
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+
     startTimer: function(){
         this.timer = setInterval(() => {
             var newCount = this.state.count - 1;
@@ -35,6 +40,9 @@ var Countdown = React.createClass({
                 count: newCount >= 0 ? newCount : 0
             });
 
+            if(newCount === 0){
+                this.setState({ countdownStatus: 'stopped' });
+            }
         }, 1000);
     },
 
